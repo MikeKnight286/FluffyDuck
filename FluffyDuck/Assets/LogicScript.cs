6060cd336a4f02ba7e8dd946aaaaa12d62ec46c4
MikeKnight286 <bhonetayzarkyaw286@gmail.com>
2023-10-23 23:54:05 +0900

    Added Score UI and Logic

---
+++ FluffyDuck/Assets/LogicScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int score){
+        playerScore = playerScore + score;
+        scoreText.text = playerScore.ToString();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
